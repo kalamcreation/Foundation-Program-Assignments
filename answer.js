07d2619a35b378ea6ef15bfa30bf09f5be2372f5
unknown <mdabulkalamosman@gmail.com>
2026-09-03 11:29:32 +0600

    feat: add validateUsername function with ordered validation

--- answer.js
+++ answer.js
@@ -33,6 +33,27 @@ function getDayType(dayName) {
   }
 }
 
-console.log(getDayType("MoNdaY")); 
+
+// Question No. 3: Username Gatekeeper
+function validateUsername(username) {
+
+  if (username.length < 4) {
+    return "Too Short";
+  }
+
+  if (username.includes(" ")) {
+    return "No Space Allowed";
+  }
+
+  if (username.toLowerCase().includes("admin")) {
+    return "Reserved Word";
+  }
+
+  return "Available";
+}
+
+console.log(validateUsername("Admin_Kalam"));
+
+
 
 
